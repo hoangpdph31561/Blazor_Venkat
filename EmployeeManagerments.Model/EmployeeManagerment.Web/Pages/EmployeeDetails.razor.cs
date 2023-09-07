6b57b85b0233ce9ba863e832e2cfc3fb4a8a5ced
Hoàng Phạm Đức <hoang23577@gmail.com>
2023-09-07 21:14:34 +0700

    Event_Onclick_OnMouseMove

--- EmployeeManagerments.Model/EmployeeManagerment.Web/Pages/EmployeeDetails.razor.cs
+++ EmployeeManagerments.Model/EmployeeManagerment.Web/Pages/EmployeeDetails.razor.cs
@@ -1,12 +1,16 @@
 ﻿using EmployeeManagerment.API.ViewModel;
 using EmployeeManagerment.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using SelfLearn_Blazor_kudvenkat.Entities;
 
 namespace EmployeeManagerment.Web.Pages
 {
     public partial class EmployeeDetails
     {
+        protected string ToaDo { get; set; }
+        protected string ButtonName { get; set; } = "Hide buttons";
+        protected string? CssClass { get; set; } = null;
         public EmployeeViewModel Employee { get; set; } = new EmployeeViewModel();
         [Inject]
         public IEmployeeService employeeService { get; set; }
@@ -15,6 +19,23 @@ namespace EmployeeManagerment.Web.Pages
         protected override async Task OnInitializedAsync()
         {
             Employee = await employeeService.GetEmployeeById(Convert.ToInt32(Id));
+        }
+        protected void GetToaDo(MouseEventArgs args)
+        {
+            ToaDo = $"X = {args.ClientX} Y = {args.ClientY}";
+        }
+        protected void HideButtonClick()
+        {
+            if(ButtonName == "Hide buttons")
+            {
+                ButtonName = "Show buttons";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                ButtonName = "Hide buttons";
+                CssClass = null;
+            }
         }
     }
 }
