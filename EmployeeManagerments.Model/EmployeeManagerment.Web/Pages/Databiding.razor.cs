f33a130bd0f4c42de2006a78a05cb6eec1da4cd1
Hoàng Phạm Đức <hoang23577@gmail.com>
2023-09-07 21:47:33 +0700

    Data-biding

---
+++ EmployeeManagerments.Model/EmployeeManagerment.Web/Pages/Databiding.razor.cs
@@ -0,0 +1,10 @@
+﻿using SelfLearn_Blazor_kudvenkat.Enum;
+
+namespace EmployeeManagerment.Web.Pages
+{
+    public partial class Databiding
+    {
+        protected string Name { get; set; } = "Đức Hoàng";
+        protected Gender Gender { get; set; } = Gender.Male;
+    }
+}
